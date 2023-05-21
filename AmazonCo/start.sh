0d89ae7e30afbb3119d005cd607f0be992b91c91
TechLion Dev (via VSCode) <git-bash@techlion.dev>
2023-05-21 06:45:54 -0400

    u
    Signed-off-by: TechLion Dev (via VSCode) <git-bash@techlion.dev>

--- AmazonCo/start.sh
+++ AmazonCo/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clear && node --max-old-space-size=16384 saveToJSON.js && node --max-old-space-size=16384 JSONtoXLSX.js
+clear && node --max-old-space-size=12288 saveToJSON.js && node --max-old-space-size=12288 JSONtoXLSX.js
